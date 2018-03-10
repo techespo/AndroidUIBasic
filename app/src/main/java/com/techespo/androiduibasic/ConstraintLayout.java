7b4740ab62dbe0cfcfab2b0e44136924742cacd0
Ashwini <espoirsoft@gmail.com>
2018-03-10 13:03:42 +0530

    Basic setup done

---
+++ app/src/main/java/com/techespo/androiduibasic/ConstraintLayout.java
@@ -0,0 +1,13 @@
+package com.techespo.androiduibasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
---
+++ app/src/main/java/com/techespo/androiduibasic/Gridlayout.java
@@ -0,0 +1,13 @@
+package com.techespo.androiduibasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gridlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridlayout);
+    }
+}
